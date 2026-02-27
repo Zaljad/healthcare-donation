5418864d933b9d4a21f9c6af225f445b25192636
Zaljad <zaina339966@gmail.com>
2026-02-27 05:55:46 +0300

    add User, MedicalEquipment, Donation, and Request models

--- models/Donation.js
+++ models/Donation.js
@@ -3,10 +3,12 @@ const mongoose = require('mongoose')
 const donationSchema = new mongoose.Schema({
   status:{type: String,
     enum: ['approved', 'pending', 'rejected'],
+    default:'pending',
     required:true
     },
-  donor: {type: mongoose.Schema.Types.ObjectId, ref: User, required:true}
+  donor: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required:true},
+  equipment: {type: mongoose.Schema.Types.ObjectId, ref: 'MedicalEquipment', required:true}
 
 },{timestamps: true})
 
-module.exports = mongoose. model('Donation', donationSchema)
+module.exports = mongoose.model('Donation', donationSchema)
--- models/Request.js
+++ models/Request.js
@@ -3,10 +3,11 @@ const mongoose = require('mongoose')
 const requestSchema = new mongoose.Schema({
   status:{type: String,
     enum: ['approved', 'pending', 'rejected'],
+    defult: 'pending',
     required:true
     },
-  requestedUser: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required:true}
-
+  requestedUser: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required:true},
+  equipment: {type: mongoose.Schema.Types.ObjectId, ref: 'MedicalEquipment', required:true}
 },{timestamps: true})
 
-module.exports = mongoose. model('Request', requestSchema)
+module.exports = mongoose.model('Request', requestSchema)
